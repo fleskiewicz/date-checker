869f8668e47c246cf66c5466763f1b7a3a27ce26
mikaszypio <szypiorowski.m@gmail.com>
2019-01-20 20:57:30 +0100

    Fix indents

--- lib/personality.rb
+++ lib/personality.rb
@@ -3,16 +3,16 @@ class Personality
   def initialize
     # Slownik cech charakteru
     @characteristics = {
-        1 => ["ambitność", "racjonalizm", "egocentryzm"],
-        2 => ["wyrozumiałość", "dyplomacja", "niepewność"],
-        3 => ["charyzma", "twórczy", "lekkomyślność"],
-        4 => ["zorganizowanie", "odpowiedzialność", "szorstkość"],
-        5 => ["śmiałość", "pomysłowość", "niecierpliwość"],
-        6 => ["wrażliwość", "zrównoważenie", "lenistwo"],
-        7 => ["szlachetność", "elegancja", "samotność"],
-        8 => ["nieustepliowść", "samokontrola", "radykalizm"],
-        9 => ["wielkoduszność", "wyrozumiałość", "wybuchowość"]
-      }
+      1 => ["ambitność", "racjonalizm", "egocentryzm"],
+      2 => ["wyrozumiałość", "dyplomacja", "niepewność"],
+      3 => ["charyzma", "twórczy", "lekkomyślność"],
+      4 => ["zorganizowanie", "odpowiedzialność", "szorstkość"],
+      5 => ["śmiałość", "pomysłowość", "niecierpliwość"],
+      6 => ["wrażliwość", "zrównoważenie", "lenistwo"],
+      7 => ["szlachetność", "elegancja", "samotność"],
+      8 => ["nieustepliowść", "samokontrola", "radykalizm"],
+      9 => ["wielkoduszność", "wyrozumiałość", "wybuchowość"]
+    }
   end
   # Metoda tworzaca stringa z wybranymi cechami charakteru
   def get_characteristics(date_value)
